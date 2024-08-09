dcb19dbbf25eb50bc341149e903f31dbb9d7909a
Kevin <hello@kevinivan.com>
2024-08-09 15:18:24 +0200

    add email sendout

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
-import { Client, Databases } from "node-appwrite"
+import { Client, Databases, Messaging, ID } from "node-appwrite"
 import axios from "axios"
 
 export default async ({ req, res, error }) => {
-  const { stakeholderId, industry, expertise, numberOfExperts } = req.body
+  const {
+    stakeholderId,
+    feedbackType,
+    industry,
+    expertise,
+    numberOfExperts,
+    timeFrame,
+    stripeLink,
+    eukapayLink,
+  } = req.body
   const slackWebhookUrl = process.env.SLACK_WEBHOOK_URL
+
+  const escapeHtml = (unsafe) => {
+    return unsafe
+      .replace(/&/g, "&amp;")
+      .replace(/</g, "&lt;")
+      .replace(/>/g, "&gt;")
+      .replace(/"/g, "&quot;")
+      .replace(/'/g, "&#039;")
+  }
 
   try {
     const client = new Client()
@@ -13,22 +31,163 @@ export default async ({ req, res, error }) => {
       .setKey(process.env.APPWRITE_API_KEY)
 
     const databases = new Databases(client)
+    const messaging = new Messaging(client)
 
-    const { firstName, lastName, email } = await databases.getDocument(
-      process.env.APPWRITE_DATABASE_ID,
-      process.env.APPWRITE_STAKEHOLDER_COLLECTION_ID,
-      stakeholderId
-    )
+    const { firstName, lastName, email, accountId } =
+      await databases.getDocument(
+        process.env.APPWRITE_DATABASE_ID,
+        process.env.APPWRITE_STAKEHOLDER_COLLECTION_ID,
+        stakeholderId
+      )
 
-    const message = `We have a new feedback request!\n- First name: ${firstName}\n- Last name: ${lastName}\n- Email: ${email}${
+    const slackMessage = `We have a new feedback request!\n- First name: ${firstName}\n- Last name: ${lastName}\n- Email: ${email}${
       industry ? `\n- Industry: ${industry}` : ""
     }${expertise ? `\n- Expertise needed: ${expertise}` : ""}${
       numberOfExperts ? `\n- Number of experts: ${numberOfExperts}` : ""
     }`
 
-    await axios.post(slackWebhookUrl, {
-      text: message,
-    })
+    await messaging.createEmail(
+      ID.unique(), // messageId
+      "Testing from server", // subject
+      `<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>TeamSpark</title>
+    <style>
+      .header {
+        max-width: 570px;
+        margin: 0 auto;
+        padding: 20px;
+      }
+      .header img {
+        max-width: 100%;
+        height: auto;
+        margin: 0;
+        padding: 0;
+        border-radius: 8px;
+      }
+      .content {
+        max-width: 570px;
+        margin: 0 auto;
+        padding: 0 20px 20px;
+        font-family: Inter, "San Francisco", Helvetica, Arial, sans-serif;
+        font-size: 15px;
+        line-height: 1.6;
+        color: #544a43;
+      }
+      .footer {
+        max-width: 570px;
+        margin: 0 auto;
+        padding: 10px 20px 20px;
+        font-family: Inter, "San Francisco", Helvetica, Arial, sans-serif;
+        font-size: 15px;
+        line-height: 1.6;
+        color: #544a43;
+      }
+      .footer img {
+        max-width: 100%;
+        height: auto;
+        margin: 0;
+        padding: 0;
+      }
+    </style>
+  </head>
+  <body>
+    <div class="header">
+      <img
+        src="https://cloud.appwrite.io/v1/storage/buckets/65dc899a9a558155ff9b/files/66b4ba630016ec477040/view?project=spark-net-app"
+        alt="teamspark"
+      />
+    </div>
+    <div class="content">
+      <p>Hello ${firstName},</p>
+      <p>
+        Thank you for your ordering a feedback session with TeamSpark!
+      </p>
+      <p>Here are the details you provided:</p>
+    <ul>
+        <li><strong>Feedback type:</strong> ${feedbackType}</li>
+        <li><strong>Industry:</strong> ${industry}</li>
+        <li><strong>Expertise requested:</strong> ${expertise}</li>
+        <li><strong>Number of experts:</strong> ${numberOfExperts}</li>
+        <li><strong>When:</strong> ${timeFrame}</li>
+    </ul>
+
+    <p>To complete your payment, please use one of the following links:</p>
+
+    <p>
+      <a
+        href="${stripeLink}"
+          target="_blank"
+          style="
+            color: #d34c1f;
+            text-decoration: underline;
+            margin: 0;
+            padding: 0;
+          ">
+        Pay with Stripe
+      </a>
+    </p>
+    <p>
+      <a
+        href="${eukapayLink}"
+        target="_blank"
+        style="
+          color: #d34c1f;
+          text-decoration: underline;
+          margin: 0;
+          padding: 0;
+      ">
+        Pay with Eukapay
+      </a>
+    </p>
+
+    <p>If you have any questions or need to make changes to your order, please don't hesitate to contact us.</p>
+
+    <p>We look forward to providing you with valuable feedback on your project!</p>
+
+    </div>
+
+    <div class="footer">
+      <p>Jason Goodman<br />Founder & CEO</p>
+      <p>
+        <img
+          src="https://cloud.appwrite.io/v1/storage/buckets/65dc899a9a558155ff9b/files/66b4be6200076caec304/view?project=spark-net-app"
+          width="90"
+          alt="jason"
+        />
+      </p>
+      <p>
+        <a
+          href="https://x.com/jgoodspark"
+          target="_blank"
+          style="
+            color: #d34c1f;
+            text-decoration: underline;
+            margin: 0;
+            padding: 0;
+          "
+          >@jgoodspark</a
+        >
+      </p>
+    </div>
+  </body>
+</html>`, // content
+      [], // topics (optional)
+      [accountId], // users (optional)
+      [], // targets (optional)
+      [], // cc (optional)
+      [], // bcc (optional)
+      false, // draft (optional)
+      true, // html (optional)
+      "" // scheduledAt (optional)
+    )
+
+    // await axios.post(slackWebhookUrl, {
+    //   text: slackMessage,
+    // })
   } catch (e) {
     error("Failed to send message: " + e.message)
     return res.send("Failed to send message")
