c3c622a6f3f8c79d00df787deb7b0ddf29d74ae7
Kevin <hello@kevinivan.com>
2024-08-10 16:58:34 +0200

    comment out email

--- index.js
+++ index.js
@@ -51,160 +51,160 @@ export default async ({ req, res, log, error }) => {
       ${numberOfExperts ? `\n- Number of experts: ${numberOfExperts}` : ""}
       ${timeFrame ? `\n- When: ${timeFrame}` : ""}`
 
-    await messaging.createEmail(
-      ID.unique(),
-      "TeamSpark Feedback Session",
-      `<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>TeamSpark</title>
-    <style>
-      .header {
-        max-width: 570px;
-        margin: 0 auto;
-        padding: 20px;
-      }
-      .header img {
-        max-width: 100%;
-        height: auto;
-        margin: 0;
-        padding: 0;
-        border-radius: 8px;
-      }
-      .content {
-        max-width: 570px;
-        margin: 0 auto;
-        padding: 0 20px 10px;
-        font-family: Inter, "San Francisco", Helvetica, Arial, sans-serif;
-        font-size: 15px;
-        line-height: 1.6;
-        color: #544a43;
-      }
-      .footer {
-        max-width: 570px;
-        margin: 0 auto;
-        padding: 0 20px 20px;
-        font-family: Inter, "San Francisco", Helvetica, Arial, sans-serif;
-        font-size: 15px;
-        line-height: 1.6;
-        color: #544a43;
-      }
-      .footer img {
-        max-width: 100%;
-        height: auto;
-        margin: 0;
-        padding: 0;
-      }
-    </style>
-  </head>
-  <body>
-    <div class="header">
-      <img
-        src="https://cloud.appwrite.io/v1/storage/buckets/65dc899a9a558155ff9b/files/66b4ba630016ec477040/view?project=spark-net-app"
-        alt="teamspark"
-      />
-    </div>
-    <div class="content">
-      <p>
-        Thank you for your ordering a feedback session with TeamSpark!
-      </p>
-      <p>Here are the details you provided:</p>
-    <ul>
-        <li><strong>Feedback type:</strong> ${feedbackType}</li>
-        <li><strong>Industry:</strong> ${industry}</li>
-        <li><strong>Expertise requested:</strong> ${expertise}</li>
-        <li><strong>Number of experts:</strong> ${numberOfExperts}</li>
-        <li><strong>When:</strong> ${timeFrame}</li>
-    </ul>
+    //     await messaging.createEmail(
+    //       ID.unique(),
+    //       "TeamSpark Feedback Session",
+    //       `<!DOCTYPE html>
+    // <html lang="en">
+    //   <head>
+    //     <meta charset="UTF-8" />
+    //     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    //     <title>TeamSpark</title>
+    //     <style>
+    //       .header {
+    //         max-width: 570px;
+    //         margin: 0 auto;
+    //         padding: 20px;
+    //       }
+    //       .header img {
+    //         max-width: 100%;
+    //         height: auto;
+    //         margin: 0;
+    //         padding: 0;
+    //         border-radius: 8px;
+    //       }
+    //       .content {
+    //         max-width: 570px;
+    //         margin: 0 auto;
+    //         padding: 0 20px 10px;
+    //         font-family: Inter, "San Francisco", Helvetica, Arial, sans-serif;
+    //         font-size: 15px;
+    //         line-height: 1.6;
+    //         color: #544a43;
+    //       }
+    //       .footer {
+    //         max-width: 570px;
+    //         margin: 0 auto;
+    //         padding: 0 20px 20px;
+    //         font-family: Inter, "San Francisco", Helvetica, Arial, sans-serif;
+    //         font-size: 15px;
+    //         line-height: 1.6;
+    //         color: #544a43;
+    //       }
+    //       .footer img {
+    //         max-width: 100%;
+    //         height: auto;
+    //         margin: 0;
+    //         padding: 0;
+    //       }
+    //     </style>
+    //   </head>
+    //   <body>
+    //     <div class="header">
+    //       <img
+    //         src="https://cloud.appwrite.io/v1/storage/buckets/65dc899a9a558155ff9b/files/66b4ba630016ec477040/view?project=spark-net-app"
+    //         alt="teamspark"
+    //       />
+    //     </div>
+    //     <div class="content">
+    //       <p>
+    //         Thank you for your ordering a feedback session with TeamSpark!
+    //       </p>
+    //       <p>Here are the details you provided:</p>
+    //     <ul>
+    //         <li><strong>Feedback type:</strong> ${feedbackType}</li>
+    //         <li><strong>Industry:</strong> ${industry}</li>
+    //         <li><strong>Expertise requested:</strong> ${expertise}</li>
+    //         <li><strong>Number of experts:</strong> ${numberOfExperts}</li>
+    //         <li><strong>When:</strong> ${timeFrame}</li>
+    //     </ul>
 
-    <p>If you didn't complete your payment, please use one of the following links:</p>
+    //     <p>If you didn't complete your payment, please use one of the following links:</p>
 
-    <p>
-      <a
-        href="${stripeLink}"
-          target="_blank"
-          style="
-            display: block;
-            width: fit-content;
-            color: #d34c1f;
-            text-decoration: underline;
-            margin: 0;
-            padding: 0;
-          ">
-        Pay with Stripe
-      </a>
-      <a
-        href="${eukapayLink}"
-        target="_blank"
-        style="
-          display: block;
-          width: fit-content;
-          color: #d34c1f;
-          text-decoration: underline;
-          margin: 0;
-          padding: 0;
-      ">
-        Pay with Crypto
-      </a>
-    </p>
+    //     <p>
+    //       <a
+    //         href="${stripeLink}"
+    //           target="_blank"
+    //           style="
+    //             display: block;
+    //             width: fit-content;
+    //             color: #d34c1f;
+    //             text-decoration: underline;
+    //             margin: 0;
+    //             padding: 0;
+    //           ">
+    //         Pay with Stripe
+    //       </a>
+    //       <a
+    //         href="${eukapayLink}"
+    //         target="_blank"
+    //         style="
+    //           display: block;
+    //           width: fit-content;
+    //           color: #d34c1f;
+    //           text-decoration: underline;
+    //           margin: 0;
+    //           padding: 0;
+    //       ">
+    //         Pay with Crypto
+    //       </a>
+    //     </p>
 
-    <p>If you have any questions or need to make changes to your order, don't hesitate to contact us.</p>
+    //     <p>If you have any questions or need to make changes to your order, don't hesitate to contact us.</p>
 
-    <p>We look forward to providing you with valuable feedback on your project!</p>
+    //     <p>We look forward to providing you with valuable feedback on your project!</p>
 
-    </div>
+    //     </div>
 
-    <div class="footer">
-      <p>Jason Goodman<br />Founder & CEO</p>
-      <p>
-        <img
-          src="https://cloud.appwrite.io/v1/storage/buckets/65dc899a9a558155ff9b/files/66b4be6200076caec304/view?project=spark-net-app"
-          width="90"
-          alt="jason"
-        />
-      </p>
-      <p>
-        <a
-          href="https://x.com/jgoodspark"
-          target="_blank"
-          style="
-            display: block;
-            width: fit-content;
-            color: #d34c1f;
-            text-decoration: underline;
-            margin: 0;
-            padding: 0;
-          "
-          >@jgoodspark</a
-        >
-        <a
-          href="https://teamspark.xyz"
-          target="_blank"
-          style="
-            display: block;
-            width: fit-content;
-            color: #d34c1f;
-            text-decoration: underline;
-            margin: 0;
-            padding: 0;
-          "
-          >teamspark.xyz</a
-        >
-      </p>
-    </div>
-  </body>
-</html>`, // content
-      [], // topics (optional)
-      [accountId], // users (optional)
-      [], // targets (optional)
-      [], // cc (optional)
-      [], // bcc (optional)
-      [], // attachments (optional)
-      false, // draft (optional)
-      true // html (optional)
-    )
+    //     <div class="footer">
+    //       <p>Jason Goodman<br />Founder & CEO</p>
+    //       <p>
+    //         <img
+    //           src="https://cloud.appwrite.io/v1/storage/buckets/65dc899a9a558155ff9b/files/66b4be6200076caec304/view?project=spark-net-app"
+    //           width="90"
+    //           alt="jason"
+    //         />
+    //       </p>
+    //       <p>
+    //         <a
+    //           href="https://x.com/jgoodspark"
+    //           target="_blank"
+    //           style="
+    //             display: block;
+    //             width: fit-content;
+    //             color: #d34c1f;
+    //             text-decoration: underline;
+    //             margin: 0;
+    //             padding: 0;
+    //           "
+    //           >@jgoodspark</a
+    //         >
+    //         <a
+    //           href="https://teamspark.xyz"
+    //           target="_blank"
+    //           style="
+    //             display: block;
+    //             width: fit-content;
+    //             color: #d34c1f;
+    //             text-decoration: underline;
+    //             margin: 0;
+    //             padding: 0;
+    //           "
+    //           >teamspark.xyz</a
+    //         >
+    //       </p>
+    //     </div>
+    //   </body>
+    // </html>`, // content
+    //       [], // topics (optional)
+    //       [accountId], // users (optional)
+    //       [], // targets (optional)
+    //       [], // cc (optional)
+    //       [], // bcc (optional)
+    //       [], // attachments (optional)
+    //       false, // draft (optional)
+    //       true // html (optional)
+    //     )
 
     await axios.post(slackWebhookUrl, {
       text: slackMessage,
